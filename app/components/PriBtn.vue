409bc2f2306cfb05dd8fce70f2b4cfb9993ddf9c
Onisodumeya Mazi <maziemmamazi@gmail.com>
2025-10-10 14:39:21 +0100

    chore: button refinement

--- app/components/PriBtn.vue
+++ app/components/PriBtn.vue
@@ -3,15 +3,23 @@
     class="text-white w-fit h-fit bg-black/60 backdrop-blur-lg text-lg rounded-full relative group border-2 border-black/60 hover:border-2 hover:border-black/60 transition-colors duration-300 ease-out"
   >
     <div
+      ref="button"
       @click.stop="buttonAction"
-      :class="`bg-orange-500 px-6 py-2 rounded-full text-nowrap ${border} transition-all duration-300 ease-in-out`"
+      @mousemove="moveGlow"
+      @mouseleave="hideGlow"
+      :class="`relative bg-orange-500 border-b-2 border-b-orange-700 border-t-2 border-t-white/50 px-6 py-2 rounded-full text-nowrap ${border} transition-all duration-300 ease-in-out overflow-hidden`"
     >
-      {{ text }}
+      <div
+        ref="glow"
+        class="absolute w-10 h-10 bg-orange-100/50 blur-md top-0 rounded-full z-10"
+      ></div>
+      <p class="relative z20">{{ text }}</p>
     </div>
   </button>
 </template>
-<script setup lang="ts">
-import { ref } from "vue";
+<script setup>
+import { ref, onMounted } from "vue";
+import gsap from "gsap";
 
 defineProps({
   text: {
@@ -27,5 +35,25 @@ function buttonAction() {
   setTimeout(() => {
     border.value = "group-hover:-translate-y-1";
   }, 150);
+}
+
+const button = ref(null);
+const glow = ref(null);
+
+function moveGlow(e) {
+  const rect = button.value.getBoundingClientRect();
+  const x = e.clientX - rect.left - 30;
+  const y = e.clientY - rect.top - 5;
+
+  gsap.to(glow.value, { x, y, duration: 0.3, ease: "power2.out" });
+  glow.value.style.opacity = 1;
+}
+
+onMounted(() => {
+  glow.value.style.opacity = 0;
+});
+
+function hideGlow() {
+  glow.value.style.opacity = 0;
 }
 </script>
